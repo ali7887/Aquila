c0f0e0095fe76e1367cc15f541438ca7cb8a7c2d
ali7887 <alikiani78@gmail.com>
2024-10-18 13:29:20 +0330

    first-commit

--- header.php
+++ header.php
@@ -1,13 +1,71 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-    
-    <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<header>
-    <h1><?php bloginfo('name'); ?></h1>
-</header>
+<?php
+/**
+ * Main template file.
+ *
+ * @package Aquila
+ */
+
+get_header();
+
+?>
+
+	<div id="primary">
+		<main id="main" class="site-main mt-5" role="main">
+			<?php
+			if ( have_posts() ) :
+				?>
+				<div class="container">
+					<?php
+					if ( is_home() && ! is_front_page() ) {
+						?>
+						<header class="mb-5">
+							<h1 class="page-title screen-reader-text">
+								<?php single_post_title(); ?>
+							</h1>
+						</header>
+						<?php
+					}
+					?>
+
+					<div class="row">
+						<?php
+						$index         = 0;
+						$no_of_columns = 3;
+
+						while ( have_posts() ) : the_post();
+
+							if ( 0 === $index % $no_of_columns ) {
+								?>
+								<div class="col-lg-4 col-md-6 col-sm-12">
+								<?php
+							}
+
+							get_template_part( 'template-parts/content' );
+
+							$index ++;
+
+							if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+								?>
+								</div>
+								<?php
+							}
+
+						endwhile;
+						?>
+					</div>
+				</div>
+			<?php
+
+			else :
+
+				get_template_part( 'template-parts/content-none' );
+
+			endif;
+
+			aquila_pagination();
+			?>
+		</main>
+	</div>
+
+<?php
+get_footer();
